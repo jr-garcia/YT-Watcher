cd6080334c25c82b2ed3cf32325c16ab6d9dface
jr-garcia <jr-garcia@users.noreply.github.com>
2018-09-08 23:55:38 -0500

    Add setup.

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='YT-Watcher', version='0.0.1', packages=['ytw'], url='', license='MIT', author='jrg', author_email='',
+      description='Youtube multi search tool', install_requires=['PySide', 'urllib3', 'certifi'])
